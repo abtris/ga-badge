c7348fca31af76addfd3d79accb2f8ff7677094a
Ladislav Prskavec <ladislav@prskavec.net>
2021-03-31 16:08:02 +0200

    fix: styles for gh badges

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var getBadge = require('../lib/ghactions.js')
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Github Action Badge Generator', url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml' });
+  res.render('index', { title: 'Github Action Badge Generator', url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml', label: 'Build Status' });
 });
 
 router.post('/', function (req, res, next) {
-  output = getBadge(req.body.gh_url)
-  res.render('badge', { title: 'Github Action Badge Generator', snippet: output, url: req.body.gh_url });
+  output = getBadge(req.body.gh_url, { title: req.body.title })
+  res.render('badge', { title: 'Github Action Badge Generator', snippet: output, url: req.body.gh_url, label: req.body.title });
 });
 
 module.exports = router;
