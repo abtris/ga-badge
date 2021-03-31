69469de5a26d6868c7ac519df052b4ef8a47f052
Ladislav Prskavec <ladislav@prskavec.net>
2021-03-31 15:36:12 +0200

    feat: add small improvement and prepare for optional options

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Github Action Badge Generator' });
+  res.render('index', { title: 'Github Action Badge Generator', url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml' });
 });
 
 module.exports = router;
