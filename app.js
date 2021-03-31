c003c3b8582f3f19fc1ed98702107803c6a50281
Ladislav Prskavec <ladislav@prskavec.net>
2021-03-31 15:49:28 +0200

    fix: simplify routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var badgeRouter = require('./routes/badge');
 
 var app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/badge', badgeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var getBadge = require('../lib/ghactions.js')
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Github Action Badge Generator', url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml' });
+});
+
+router.post('/', function (req, res, next) {
+  output = getBadge(req.body.gh_url)
+  res.render('badge', { title: 'Github Action Badge Generator', snippet: output, url: req.body.gh_url });
 });
 
 module.exports = router;
