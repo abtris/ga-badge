811e8358dbf516b2c775323a407583955e6970ba
Ladislav Prskavec <ladislav@prskavec.net>
2021-04-01 10:33:54 +0200

    refactor: remove badge view and use just index, add defaults, support for branch

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var getBadge = require('../lib/ghactions.js')
 
 
+const defaultSnippet = '[![Build Status](https://github.com/abtris/ga-badge/actions/workflows/node.js.yml/badge.svg)](https://github.com/abtris/ga-badge/actions)'
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Badge Generator', url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml', label: 'Build Status' });
+  res.render('index', { title: 'Badge Generator', snippet: defaultSnippet, url: 'https://github.com/abtris/ga-badge/actions/workflows/node.js.yml', label: 'Build Status', branch: 'default' });
 });
 
 router.post('/', function (req, res, next) {
-  output = getBadge(req.body.gh_url, { title: req.body.title })
-  res.render('badge', { title: 'Badge Generator', snippet: output, url: req.body.gh_url, label: req.body.title });
+  output = getBadge(req.body.gh_url, { title: req.body.title, branch: req.body.branch })
+  res.render('index', { title: 'Badge Generator', snippet: output, url: req.body.gh_url, label: req.body.title, branch: req.body.branch });
 });
 
 module.exports = router;
