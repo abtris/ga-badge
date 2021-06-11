5fc69b2dcee47f5d36ef40b9a46ad3234be9b211
Ladislav Prskavec <ladislav@prskavec.net>
2021-06-11 19:52:55 +0200

    feat: add honeycomb

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var HONEYCOMB_API_KEY = process.env.HONEYCOMB_API_KEY
+require("honeycomb-beeline")({
+  // Get this via https://ui.honeycomb.io/account after signing up for Honeycomb
+  writeKey: HONEYCOMB_API_KEY,
+  // The name of your app is a good choice to start with
+  dataset: "my-node-distributed-app",
+  serviceName: "my-node-service"
+});
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -21,12 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
